8c84292d54a0b3aed0bce83f7565ffdc17515abb
Edward Son <90419532+nahyukee@users.noreply.github.com>
2021-12-01 23:45:13 -0800

    Initial commit

---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        [Required]
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MvcMovie
+ Asp.Net Core 6.0 MVC
